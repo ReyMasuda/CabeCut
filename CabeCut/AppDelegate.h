5e20b1ff69e830d028eeb03cc21c59b88fff2945
Toru Masuda <TMR@Toru-Masuda-no-MacBook.local>
2014-11-15 19:22:44 +0900

    Initial Commit

---
+++ CabeCut/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CabeCut
+//
+//  Created by Toru Masuda on 2014/11/15.
+//  Copyright (c) 2014年 Rey Masuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CabeCut/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CabeCut
+//
+//  Created by Toru Masuda on 2014/11/15.
+//  Copyright (c) 2014年 Rey Masuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
