1f228009cee0675769cb521e9896825a8aaa0f18
ReyMasuda <quit9909@icloud.com>
2014-11-22 17:52:03 +0900

    がんばったよ 1スコアつくった 2キャベツの画像つくった 3カウントとキャベツの関連付け中

--- CabeCut/ViewController.h
+++ CabeCut/ViewController.h
@@ -11,7 +11,16 @@
 @interface ViewController : UIViewController{
     int number;
     IBOutlet UILabel *label;
-    
+    UIImage *image1;
+    UIImage *image2;
+    UIImage *image3;
+    UIImage *image4;
+    UIImage *image5;
+    UIImage *image6;
+    UIImage *image7;
+    UIImage *image8;
+    UIImage *image9;
+    UIImage *image10;
 }
 
 -(IBAction)cabeButton;
--- CabeCut/ViewController.m
+++ CabeCut/ViewController.m
@@ -31,12 +31,52 @@
 -(IBAction)cabeButton{
     number=number -1;
     label.text = [NSString stringWithFormat:@"%d",number];
+    if (number=number -1) {
+        UIImage *image1 = [UIImage imageNamed:@"cabe1"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image2 = [UIImage imageNamed:@"cabe2"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image3 = [UIImage imageNamed:@"cabe3"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image4 = [UIImage imageNamed:@"cabe4"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image5 = [UIImage imageNamed:@"cabe5"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image6 = [UIImage imageNamed:@"cabe6"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image7 = [UIImage imageNamed:@"cabe7"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image8 = [UIImage imageNamed:@"cabe8"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image9 = [UIImage imageNamed:@"cabe9"];
+    }
+    
+    if (number=number -1) {
+        UIImage *image10 = [UIImage imageNamed:@"cabe10"];
+    }
     
 }
 
 -(IBAction)ReloadButton{
     number=10;
     label.text = [NSString stringWithFormat:@"%d",number];
+    
 }
 
 
